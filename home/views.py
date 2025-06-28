a26847e40a1e4ae99f5276e19681dd3db0455b5c
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2025-06-28 10:07:04 +0600

    Added new use data or login system is working usename base

--- home/views.py
+++ home/views.py
@@ -15,34 +15,22 @@ def home(req):
     img = Image.objects.all() # Fetch all Image objects from the database.
     return render(req, 'home/home.html', {'img':img, 'form': form}) 
 
+
 def login_view(req):
     if req.method == 'POST':
-        log_email = req.POST.get('email')
-        log_pass = req.POST.get('password')
-        
-        user = authenticate(req, email=log_email, password = log_pass) # Authenticate the user with the provided email and password.
-        if user is not None: # If the user is authenticated, proceed to log them in.
-            login(req, user) # If the user is authenticated, log them in.
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+
+        user = authenticate(req, username=username, password=password)
+        if user is not None:
+            login(req, user)
             return redirect('home')
         else:
-            messages.error(req, 'Invalid credentials! Please try again.') # If authentication fails, show an error message.
-            return redirect('login') 
+            messages.error(req, 'Invalid username or password!')
+            return redirect('login')
+    
     return render(req, 'home/login.html')
 
-# def signup(req):
-#     if req.method == 'POST':
-#         E_mail = req.POST.get('email') # Get the email from the POST request. 
-#         pass1 = req.POST.get('password1') # Get the first password from the POST request.
-#         pass2 = req.POST.get('password2') # Get the second password from the POST request.
-#         if pass1 != pass2: # Check if the two passwords match.
-#             messages.error(req, 'Passwords do not match!') # If the passwords do not match, show an error message.
-#             return redirect('signup')
-#         else:    
-#             my_user = User.objects.create_user(E_mail,pass1,pass2) # Create a new user with the provided email and password.
-#             my_user.save() # Save the new user to the database.
-#             messages.success(req, 'Account created successfully!') # Show a success message.
-#         return redirect('login')
-#     return render(req, 'home/signup.html')
 
 def signup(req):
     if req.method == 'POST':
